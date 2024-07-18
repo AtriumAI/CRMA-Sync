eb6b8370aceac7e82ab167e4565fe0095b475bff
jasonhalpern <jason@atrium.ai>
2024-07-18 14:01:10 -0400

    Add debugging, target branch option

--- metadata-sync.sh
+++ metadata-sync.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 set -o errexit -o pipefail -o noclobber -o nounset
 export PATH=$(pwd)/node_modules/.bin:$PATH
-sf version
+if [[ $DEBUG_LOGGING ]]; then
+  sf version
+fi
 
 # Salesforce Org Credentials
 ORG_ALIAS="SFOrg"
 
 # Git Repository Settings
 GIT_REPO_PATH=$(pwd)  # Path to Repo on local machine
-GIT_BRANCH="main"
 
 # Salesforce Metadata Types to Retrieve
 METADATA_TYPES=("WaveApplication" "WaveDashboard" "WaveDataflow" "WaveDataset" "WaveLens" "WaveRecipe" "WaveTemplateBundle" "WaveXmd")
 
 # Salesforce CLI Login
-echo "Logging in to Salesforce org..."
+if [[ $DEBUG_LOGGING ]]; then
+  echo "Logging in to Salesforce org..."
+fi
 echo $SFDX_AUTH_URL > sfdx_auth.txt
 sf org login sfdx-url --sfdx-url-file sfdx_auth.txt --set-default --alias $ORG_ALIAS
 rm sfdx_auth.txt
 
 # Check if API_VERSION was specified
 if [[ $API_VERSION != '' ]]; then
-  echo "INFO: API_VERSION $API_VERSION specified. will use that unless overridden by sfdx-project.json sourceApiVersion"
+  if [[ $DEBUG_LOGGING ]]; then
+    echo "INFO: API_VERSION $API_VERSION specified. will use that unless overridden by sfdx-project.json sourceApiVersion"
+  fi
   API_VERSION="--api-version $API_VERSION"
 else
   echo "WARN: API_VERSION not specified. Will use sourceApiVersion from sfdx-project.json, or current latest API version if that is unavailable"
@@ -32,18 +37,22 @@ git pull
 
 # Retrieve Metadata from Salesforce Org
 for TYPE in "${METADATA_TYPES[@]}"; do
-  echo "Retrieving $TYPE metadata..."
+  if [[ $DEBUG_LOGGING ]]; then
+    echo "INFO: Retrieving $TYPE metadata..."
+  fi
   sf project retrieve start --metadata "$TYPE" $API_VERSION
 done
 
 # Git Commit and Push
-##echo "Adding changes to Git..."
+echo "Adding changes to Git (Branch: $GIT_BRANCH)..."
 git -C "$GIT_REPO_PATH" add .
-git -C "$GIT_REPO_PATH" commit -m "Update Salesforce metadata - $(date)"
+git -C "$GIT_REPO_PATH" commit -m "Update CRMA metadata - $(date)"
 git -C "$GIT_REPO_PATH" push origin "$GIT_BRANCH"
 
 # Salesforce CLI Logout
-echo "Logging out from Salesforce org..."
+if [[ $DEBUG_LOGGING ]]; then
+  echo "Logging out from Salesforce org..."
+fi
 sf org logout --target-org $ORG_ALIAS --no-prompt
 
 echo "Script completed successfully!"
